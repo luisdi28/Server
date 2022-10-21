a1809f4de8d9eb2286b3bb18a4879b5184061736
luis <luisdi.28@estudiantec.cr>
2022-10-20 21:23:50 -0600

    Mejoras que permitirían hacer las pruebas unitarias

---
+++ tst/cmake-build-debug/CMakeFiles/servidor.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for servidor.
+# This may be replaced when dependencies are built.
---
+++ tst/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/luis/CLionProjects/Server/tst/lib/googletest/googlemock
+# Build directory: /home/luis/CLionProjects/Server/tst/cmake-build-debug/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")

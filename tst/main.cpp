7c812e336bc59e0c9781c47196f2c6ab1414ef3d
luis <luisdi.28@estudiantec.cr>
2022-10-20 21:22:47 -0600

    Mejoras que permitirían hacer las pruebas unitarias

--- tst/main.cpp
+++ tst/main.cpp
@@ -1,4 +1,83 @@
 #include "gtest/gtest.h"
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+
+#define PORT 8080
+
+using namespace std;
+
+class Server{
+
+    void método() {
+        int server_fd, new_socket, valread;
+        struct sockaddr_in address;
+        int opt = 1;
+        int addrlen = sizeof(address);
+        char buffer[20] = {0};
+
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0))
+            == 0) {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Forcefully attaching socket to the port 8080
+        if (setsockopt(server_fd, SOL_SOCKET,
+                       SO_REUSEADDR | SO_REUSEPORT, &opt,
+                       sizeof(opt))) {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(PORT);
+
+        // Forcefully attaching socket to the port 8080
+        if (bind(server_fd, (struct sockaddr *) &address,
+                 sizeof(address))
+            < 0) {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        if (listen(server_fd, 3) < 0) {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket
+                     = accept(server_fd, (struct sockaddr *) &address,
+                              (socklen_t *) &addrlen))
+            < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        valread = read(new_socket, buffer, 1024);
+
+        //send(new_socket, hello, strlen(hello), 0);
+        for (int i = 0; i < 20; i++) {
+            cout << buffer[i] << endl;
+        }
+
+        close(new_socket);
+        shutdown(server_fd, SHUT_RDWR);
+    }
+
+public:
+    int método2(int a, int b){
+        return a+b;
+    }
+};
+
+TEST(SeverTEST, Test1){
+    Server servidor;
+
+    EXPECT_EQ(servidor.método2(5,5),10);
+}
 
 int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, argv);

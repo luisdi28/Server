5a9cac9814cd8f7ba06d2bf514b442525fdf61b5
luisdi28 <luisdi.28@estudiantec.cr>
2022-10-28 22:40:02 -0600

    Revert "Mejoras que permitirían hacer las pruebas unitarias"
    This reverts commit a1809f4de8d9eb2286b3bb18a4879b5184061736.

--- tst/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luis/CLionProjects/Server/tst
-# Build directory: /home/luis/CLionProjects/Server/tst/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/googletest")
